f7438e584ac3ed3fc8afe3ab5f6704d16f90f041
vanko2012 <103481948+vanko2012@users.noreply.github.com>
2023-03-01 11:14:44 +0200

    first commit

---
+++ Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparePersons
+{
+    public class Person:IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }   
+        }
+        public Person(string name,int age,string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{this.Name}, {this.Age} years, from {this.town}");
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result =Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result=Age.CompareTo(other.Age);
+            }
+            else
+            {
+                result=Town.CompareTo(other.Town);  
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparePersons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("IVAN", 23, "PLOVDIV");
+            Person p2 = new Person("MARIYA", 18, "SOFIYA");
+            Console.WriteLine(p1.CompareTo(p2));
+        }
+    }
+}
